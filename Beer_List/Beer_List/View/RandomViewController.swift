a04252d840ac7ced389c31eda394cf5cd13afe9d
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-25 21:25:11 +0900

    [Add] BasicUI

--- Beer_List/Beer_List/View/RandomViewController.swift
+++ Beer_List/Beer_List/View/RandomViewController.swift
@@ -5,4 +5,26 @@
 //  Created by DohyunKim on 2021/03/25.
 //
 
-import Foundation
+import UIKit
+
+class RandomViewController: UIViewController {
+    
+    @IBOutlet weak var beerInfoTextView: UITextView!
+    @IBOutlet weak var randomButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UiExtention()
+        
+    }
+    
+    
+    
+}
+
+extension RandomViewController {
+    
+    func UiExtention() {
+        randomButton.layer.cornerRadius = 10
+    }
+}
--- Beer_List/Beer_List/View/SearchViewController.swift
+++ Beer_List/Beer_List/View/SearchViewController.swift
@@ -5,4 +5,13 @@
 //  Created by DohyunKim on 2021/03/25.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
