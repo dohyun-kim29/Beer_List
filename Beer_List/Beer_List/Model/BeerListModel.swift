110e241f5588126299a4e1af1c5a807fb0a78c49
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-26 09:00:52 +0900

    [Add] BeerListModel

---
+++ Beer_List/Beer_List/Model/BeerListModel.swift
@@ -0,0 +1,30 @@
+//
+//  BeerListModel.swift
+//  Beer_List
+//
+//  Created by DohyunKim on 2021/03/26.
+//
+
+import Foundation
+
+struct BeerListModel: Codable {
+    
+    let id: Int
+    let name: String
+//    let tagline: String
+//    let first_brewed: String
+    let description: String
+    let image_url: String
+//    let abv: Int
+//    let ibu: Int
+//    let target_fg: Int
+//    let target_og: Int
+//    let ebc: Int
+//    let srm: Int
+//    let ph: Int
+//    let attenuation_level: Int
+//    let volume:
+//    let boil_volume:
+//    let method:
+    
+}
