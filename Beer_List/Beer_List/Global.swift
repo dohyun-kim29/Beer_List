32069e6cd7cbded6d45f0049e6a4e9353f70ad42
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-26 14:16:43 +0900

    [Modify] ListModel, Global

--- Beer_List/Beer_List/Global.swift
+++ Beer_List/Beer_List/Global.swift
@@ -8,3 +8,4 @@
 import Foundation
 
 let mainUrl = "https://api.punkapi.com/v2/beers"
+let decoder = JSONDecoder()
--- Beer_List/Beer_List/Model/BeerListModel.swift
+++ Beer_List/Beer_List/Model/BeerListModel.swift
@@ -7,24 +7,86 @@
 
 import Foundation
 
+
+
 struct BeerListModel: Codable {
     
-    let id: Int
-    let name: String
-//    let tagline: String
-//    let first_brewed: String
-    let description: String
-    let image_url: String
-//    let abv: Int
-//    let ibu: Int
-//    let target_fg: Int
-//    let target_og: Int
-//    let ebc: Int
-//    let srm: Int
-//    let ph: Int
-//    let attenuation_level: Int
-//    let volume:
-//    let boil_volume:
-//    let method:
+    var id: Int?
+    var name: String?
+    var tagline: String?
+    var first_brewed: String?
+    var description: String?
+    var image_url: String?
+    var abv: Double?
+    var ibu: Int?
+    var target_fg: Int?
+    var target_og: Int?
+    var ebc: Int?
+    var srm: Int?
+    var ph: Double?
+    var attenuation_level: Int?
+    var volume: Volume?
+    var boil_volume: Boil_Volume?
+    var method: Method?
+    var fermentation: Fermentation?
+    var ingredients: Ingredients?
+    var food_pairing: Array<String>?
+    var brewers_tips: String?
+    var contributed_by: String?
+
+}
+
+struct Volume: Codable {
+    var value: Int?
+    var unit: String?
+}
+
+struct Boil_Volume: Codable {
+    var value: Int?
+    var unit: String?
+}
+
+struct Method: Codable {
+    var mash_temp: Temp?
+    var duration: Int?
+}
+
+struct Temp: Codable {
+    var value: Int?
+    var unit: String?
+}
+
+struct Fermentation: Codable {
+    var temp: Temp?
+    var twist: String
+}
+
+struct Ingredients: Codable {
+    var malt: Malt?
+    var hops: Array<Hops>?
+    var yeast: String?
     
 }
+
+struct Malt: Codable {
+    var name: String?
+    var amount: Amount?
+}
+
+struct Amount: Codable {
+    var value: Int?
+    var unit: String?
+}
+
+struct Hops: Codable {
+    var name: String?
+    var amount: Amount?
+    var add: String?
+    var attribute: String?
+    
+}
+
+
+
+
+
